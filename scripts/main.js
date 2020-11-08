2a90fb640e078fba81f8e98979377b4b6beb7f49
Daniel Rojas <53954296+danni3231@users.noreply.github.com>
2020-11-08 17:52:50 -0500

    Final
    se acabo esta mamada xd

--- scripts/main.js
+++ scripts/main.js
@@ -4,10 +4,9 @@ const videos = document.querySelectorAll('.videos');
 
 videos.forEach(element => {
     console.log("yolo");
-    
+
     element.addEventListener('click', function(){
         screen.src = `./data/videos/${element.innerText}.mp4`;
         screen.play();
     })
 });
-
