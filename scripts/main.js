b309b2801be13130183580a71b0733b8119ce29f
Daniel Rojas <53954296+danni3231@users.noreply.github.com>
2020-11-08 17:26:32 -0500

    primera pagina
    se agrego las animaciones para la primer pagina xd

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const screen = document.querySelector('.screen');
+
+const videos = document.querySelectorAll('.videos');
+
+videos.forEach(element => {
+    console.log("yolo");
+    
+    element.addEventListener('click', function(){
+        screen.src = `./data/videos/${element.innerText}.mp4`;
+        screen.play();
+    })
+});
+
